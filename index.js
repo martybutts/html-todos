4c4fa3f6cc56ca290645645755bb7a46dc5b1808
Martin <martinbutts@clear.net.nz>
2016-10-28 15:44:10 +1300

    work on show function

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ function add() {
 
   return false
 }
+
+function show() {
+  var todos = getTodos()
+
+  var html = '<ul>'
+  for(var i=0; i<todos.length; i++) {
+    html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">x</button></li>'
+  }
+  html += '</ul>'
+
+  documsnt.getElementById('todos').innerHTML = html
+
+  var buttons = document.getElementsByClassName('remove')
+  for (var i=0; i<butons.length; i++) {
+    buttons[i].addEventListener('click', remove)
+  }
+}
